cef10ea46ccd89599506bcac7b080b26d37c2b35
alfa0805 <asadbekarslonbekov53@gmail.com>
2025-06-15 10:44:35 +0500

    swiper res

--- src/components/Swiper/Swipers.jsx
+++ src/components/Swiper/Swipers.jsx
@@ -71,23 +71,26 @@ export default function SwiperSlides() {
                 backgroundSize: "cover",
                 backgroundPosition: "center",
               }}
-              className="h-[100dvh] w-full px-10 py-20 text-white"
+              className="h-[100dvh] w-full px-10 py-20 text-white relative"
             >
-              <div className="max-w-7xl h-full mx-auto">
-                <div className="w-[250px] mt-8 border-b-2 border-r-2 border-[#b9fa15] rounded-[50%] px-7 py-4">
+              <div className="max-w-7xl h-full mx-auto flex flex-col max-sm:items-center gap-6">
+
+                <span className="w-full h-full min-md:hidden bg-black/40 absolute top-0 left-0"></span>
+
+                <div className="w-[250px] z-10 max-sm:hidden mt-3 border-b-2 border-r-2 border-[#b9fa15] rounded-[50%] px-7 py-4">
                   <p className="text-xl text-right">
                     O'zbekiston bo'ylab yetkazib berish!
                   </p>
                 </div>
-                <h1 className="w-[600px] text-4xl font-medium mt-5">
+                <h1 className="min-md:w-[600px] z-10 max-md:text-2xl max-sm:text-xl max-sm:text-center text-4xl font-medium mt-5">
                   {car?.title}
                 </h1>
-                <p className="text-2xl font-medium text-[#595757] mt-5">
+                <p className="text-2xl z-10 max-md:text-xl max-sm:text-xl max-sm:text-center font-medium text-[#595757]">
                   {car?.text}
                 </p>
                 <NavLink>
-                  <button className="mt-12 bg-[#00000099] text-white relative overflow-hidden group border-2 border-[#b9fa15] px-7 py-4 transform duration-500 rounded-full hover:text-black flex items-center gap-3">
-                    <span className="z-20 text-xl">KATALOGGA O‘TISH</span>{" "}
+                  <button className="mt-5 bg-[#000000a6] text-white relative overflow-hidden group border-2 border-[#b9fa15] px-7 py-4 max-sm:py-2 transform duration-500 rounded-full hover:text-black flex items-center gap-3">
+                    <span className="z-20 text-xl max-sm:text-sm">KATALOGGA O‘TISH</span>{" "}
                     <span className=" z-20 text-2xl ">↗</span>
                     <span className="w-full h-full z-10 absolute -left-72 bg-[#b9fa15]  group-hover:left-0 transform duration-500 transition-all"></span>
                   </button>
@@ -96,19 +99,18 @@ export default function SwiperSlides() {
             </div>
           </SwiperSlide>
         ))}
-        <div className="w-[40px] h-[40px] rounded-[50%] border custom-prev absolute right-25 bottom-20 ">{`<`}</div>
         {/* Custom Arrow Buttons */}
-        <div className="custom-prev absolute right-35 bottom-16 w-[60px] h-[60px] rounded-[50%] -translate-y-1/2 z-10 cursor-pointer border text-xl font-bold bg-black/70 text-white hover:bg-[#323131d3] hover:text-[#b9fa15] transition flex items-center justify-center">
+        <div className="custom-prev absolute max-lg:hidden right-35 bottom-16 w-[60px] h-[60px] rounded-[50%] -translate-y-1/2 z-10 cursor-pointer border text-xl font-bold bg-black/70 text-white hover:bg-[#323131d3] hover:text-[#b9fa15] transition flex items-center justify-center">
           {/* <FaArrowLeft /> */}
           {`<`}
         </div>
-        <div className="custom-next absolute right-15 bottom-16 w-[60px] h-[60px] -translate-y-1/2 z-10 cursor-pointer border text-xl font-bold bg-black/70 text-white p-3 rounded-full hover:bg-[#323131d3] hover:text-[#b9fa15] transition flex items-center justify-center">
+        <div className="custom-next absolute max-lg:hidden right-15 bottom-16 w-[60px] h-[60px] -translate-y-1/2 z-10 cursor-pointer border text-xl font-bold bg-black/70 text-white p-3 rounded-full hover:bg-[#323131d3] hover:text-[#b9fa15] transition flex items-center justify-center">
           {`>`}
           {/* <FaArrowRight /> */}
         </div>
 
         {/* Custom Pagination */}
-        <div className="custom-pagination absolute bottom-20 left-10 z-10"></div>
+        <div className="custom-pagination max-md:hidden absolute bottom-20 left-10 z-10 max-lg:left-1/2 max-lg:bottom-16"></div>
       </Swiper>
     </div>
   );
