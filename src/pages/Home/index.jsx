247d6e02fa817de55af8da985a8c4a836a3a952d
alfa0805 <asadbekarslonbekov53@gmail.com>
2025-06-16 02:14:41 +0500

    category_card

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import Catalog from '../../components/Catalog/Catalog'
 import HomeNews from '../../components/News/HomeNews'
 import SwiperSlides from '../../components/Swipers/Swipers'
 
@@ -5,6 +6,7 @@ const HomePage = () => {
   return (
     <div>
       <SwiperSlides />
+      <Catalog/>
       <HomeNews />
     </div>
   )
