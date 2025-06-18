f43f2f7fe031f5eae29404951055f04b9f8cc0ec
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2025-06-19 01:46:56 +0500

    Category and news store added

---
+++ src/store/category.js
@@ -0,0 +1,37 @@
+import { create } from "zustand";
+import { axiosInstance } from "@/lib/axiosIntance";
+
+export const useCategoryStore = create((set) => ({
+    categories: [],
+    selectedCategory: null,
+    loading: false,
+    error: null,
+
+    // Get all categories
+    fetchCategories: async () => {
+        set({ loading: true, error: null });
+        try {
+            const res = await axiosInstance.get("/api/categories");
+            set({ categories: res.data.data });
+        } catch (err) {
+            set({ error: err.message });
+        } finally {
+            set({ loading: false });
+        }
+    },
+
+    // Get one category by ID
+    getCategoryById: async (id) => {
+        set({ loading: true, error: null });
+        try {
+            const res = await axiosInstance.get(`/api/categories/${id}`);
+            set({
+                selectedCategory: res.data.data,
+            });
+        } catch (err) {
+            set({ error: err.message });
+        } finally {
+            set({ loading: false });
+        }
+    },
+}));
