9c8737e8a0bdd6aaa4ec3ea142871dd7889bd72a
alfa0805 <asadbekarslonbekov53@gmail.com>
2025-06-23 16:11:58 +0500

    gg

--- src/pages/Category/components/MassageChairGrid.jsx
+++ src/pages/Category/components/MassageChairGrid.jsx
@@ -5,44 +5,37 @@ import { useTranslation } from "react-i18next";
 const MassageChairGrid = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
-    const { t , i18n } = useTranslation();
-    const lang = ["uz", "ru"].includes(i18n.language) ? i18n.language : "uz";
+
+  const { i18n } = useTranslation();
+  const lang = ["uz", "ru"].includes(i18n.language) ? i18n.language : "uz";
 
   const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
-useEffect(() => {
-  fetch(`${baseUrl}/api/products`)
-    .then((res) => res.json())
-    .then((data) => {
-      const searchKeywords = [
-        "uqalash uskunalari",
-        "массажное оборудование",
-      ];
-
-      const filtered = (data?.data || []).filter((product) => {
-        const categoryName = product?.category?.name?.[lang]?.toLowerCase();
-
-        return searchKeywords.some((keyword) =>
-          categoryName?.includes(keyword)
-        );
+  useEffect(() => {
+    fetch(`${baseUrl}/api/products`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data?.data || []);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("Xatolik:", err);
+        setLoading(false);
       });
+  }, []);
 
-      setProducts(filtered);
-      setLoading(false);
-    })
-    .catch((err) => {
-      console.error("Xatolik:", err);
-      setLoading(false);
-    });
-}, [lang]);
-   
+  const filteredProducts = products.filter((product) => {
+    const categoryName = product?.category?.name?.[lang]?.toLowerCase();
+    return categoryName === "uqalash uskunalari" || categoryName === "массажное оборудование";
+  });
+
   return (
     <div className="grid lg:grid-cols-4 md:grid-cols-3 lg:gap-7 grid-cols-2 sm:gap-4 gap-3">
       {loading ? (
         <p>Yuklanmoqda...</p>
-      ) : products.length > 0 ? (
-        products.map((product) => (
-          <MassageCategoryCard key={product.id} product={{ product }} />
+      ) : filteredProducts.length > 0 ? (
+        filteredProducts.map((product) => (
+          <MassageCategoryCard key={product.id} product={product} />
         ))
       ) : (
         <p>Massaj uskunalari topilmadi.</p>
